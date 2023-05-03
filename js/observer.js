9989e4a561d44f685002b9afa4de17a0c7f7734d
youknowicexy <120013279+youknowicexy@users.noreply.github.com>
2023-05-03 15:11:02 +0700

    Add files via upload

---
+++ js/observer.js
@@ -0,0 +1,14 @@
+const fadeIns = document.querySelectorAll('.fade-in');
+
+const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('active');
+            observer.unobserve(entry.target);
+        }
+    });
+});
+
+fadeIns.forEach(fadeIn => {
+    observer.observe(fadeIn);
+});
