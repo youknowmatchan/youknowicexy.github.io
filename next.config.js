5051ae50587cf226e888145b0b54d7b2883747d4
matchan <120013279+youknowmatchan@users.noreply.github.com>
2025-03-23 14:40:10 +1300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,12 +4,7 @@ module.exports = {
         config.module.rules.push({
             test : /\.svg$/,
             use : '@svgr/webpack',
-        })
-        return config
-    }
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
- }
+        });
+        return config;
+    },
 }
